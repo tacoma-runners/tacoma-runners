c9c515a61371329aa327201be52465d827e95592
Todd Detweiler <todddetweiler@Todds-MacBook-Pro.local>
2023-12-29 15:46:12 -0800

    feat: Add newer logic for how upcoming run fetching works

--- api/src/runs/runs.service.ts
+++ api/src/runs/runs.service.ts
@@ -22,18 +22,33 @@ export class RunsService {
 
   async findUpcoming(runType = null): Promise<Run> {
     // Look for next run but with 6 hour buffer after last one
-    const dateEnd = new Date(new Date().setHours(new Date().getHours() + 162));
-    const dateStart = new Date(new Date().setDate(new Date().getDate() - 60));
+    const dateEnd = new Date(new Date().setHours(new Date().getHours() + 164));
+    const dateStart = new Date(new Date().setHours(new Date().getHours() - 4));
 
-    const test = await this.runsReposity.find({
+    let runs = await this.runsReposity.find({
       where: {
         eventDate: Between(dateStart, dateEnd),
         ...(runType && { runType }),
       },
-      order: { eventDate: 'DESC' },
+      order: { eventDate: 'ASC' },
       take: 1,
     });
-    return test[0];
+
+    if (runs.length === 0) {
+      const newDateStart = new Date(
+        new Date().setDate(new Date().getDate() - 60),
+      );
+      runs = await this.runsReposity.find({
+        where: {
+          eventDate: Between(newDateStart, dateEnd),
+          ...(runType && { runType }),
+        },
+        order: { eventDate: 'DESC' },
+        take: 1,
+      });
+    }
+
+    return runs[0];
   }
 
   findOne(id: number): Promise<Run> {
