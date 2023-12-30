97142982617fe902b8e06e41cf866ed1541f5631
Todd Detweiler <todddetweiler@Todds-MacBook-Pro.local>
2023-12-29 16:08:43 -0800

    feat: Add logic to make sure cors is enabled

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://https://tacoma-runners-api.vercel.app/',
+      'http://localhost:3000',
+    ],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
