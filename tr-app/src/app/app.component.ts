d243ed1a5adea74079d9a90f68957399d952b94e
tdetweiler <t.detweiler13@gmail.com>
2025-02-09 14:21:23 -0800

    feat: Remove giving tuesday banner and update pnpm

--- tr-app/src/app/app.component.ts
+++ tr-app/src/app/app.component.ts
@@ -1,23 +1,46 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterModule, Router, Event as NavigationEvent, NavigationEnd } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterModule,
+  Router,
+  Event as NavigationEvent,
+  NavigationEnd,
+} from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 import { MaterialModule } from './material/material.module';
 import { Observable } from 'rxjs';
-import { NavigationComponent } from "./components/navigation/navigation.component";
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFacebookF, faStrava, faInstagram, faMeetup } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebookF,
+  faStrava,
+  faInstagram,
+  faMeetup,
+} from '@fortawesome/free-brands-svg-icons';
 import { Platform } from '@angular/cdk/platform';
 import { ShowonceDialogComponent } from './components/showonce-dialog/showonce-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [CommonModule, MaterialModule, RouterOutlet, RouterModule, NavigationComponent, FontAwesomeModule, ShowonceDialogComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterOutlet,
+    RouterModule,
+    NavigationComponent,
+    FontAwesomeModule,
+    ShowonceDialogComponent,
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'Tacoma Runners';
@@ -32,37 +55,36 @@ export class AppComponent implements OnInit {
     .observe([
       Breakpoints.Handset,
       Breakpoints.TabletPortrait,
-      Breakpoints.WebPortrait
+      Breakpoints.WebPortrait,
     ])
     .pipe(map((result: BreakpointState) => result.matches));
 
-  public isLoaded:boolean = false;
+  public isLoaded: boolean = false;
 
-  constructor(private observer: BreakpointObserver,
+  constructor(
+    private observer: BreakpointObserver,
     private router: Router,
     private platform: Platform,
-    public openOnceDialog: MatDialog) {}
+    public openOnceDialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.isLoaded = true;
 
     if (!this.platform.IOS) {
-      this.router.events.pipe(
-        filter(event => event instanceof NavigationEnd)
-      )
-      .subscribe((e: NavigationEvent) => {
-        document.getElementsByTagName("router-outlet")[0].scrollIntoView();
-      });
+      this.router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .subscribe((e: NavigationEvent) => {
+          document.getElementsByTagName('router-outlet')[0].scrollIntoView();
+        });
     }
-
-    this.openLocationDialog();
   }
 
   openLocationDialog(): void {
     const dialogRef = this.openOnceDialog.open(ShowonceDialogComponent, {
       minWidth: '50%',
       disableClose: false,
-      hasBackdrop: true
+      hasBackdrop: true,
     });
   }
 }
